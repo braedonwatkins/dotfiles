5aa33aeea7dba354b4386738a7faaa0341a73aa4
braedonwatkins <watkins.braedon@gmail.com>
2025-04-05 19:04:04 -0400

    recent files keymap

--- lua/braedon/plugins/telescope.lua
+++ lua/braedon/plugins/telescope.lua
@@ -3,9 +3,11 @@ local telescope = require("telescope.builtin")
 vim.keymap.set("n", "<leader>pf", function()
 	telescope.find_files({})
 end)
-
 vim.keymap.set("n", "<leader>ps", function()
 	telescope.live_grep({})
+end)
+vim.keymap.set("n", "<leader>r", function()
+	telescope.oldfiles({})
 end)
 
 vim.keymap.set("n", "<C-p>", telescope.git_files, { desc = "Find files tracked by Git" })
