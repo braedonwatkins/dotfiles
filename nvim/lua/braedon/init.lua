743870298d4488387845f5a34ab119d6bcf99660
braedonwatkins <watkins.braedon@gmail.com>
2025-04-06 10:36:55 -0400

    fix annoying oil always open issue

--- nvim/lua/braedon/init.lua
+++ nvim/lua/braedon/init.lua
@@ -4,4 +4,19 @@ require("braedon.settings")
 require("braedon.plugins.lsp")
 
 require("oil").setup()
-vim.cmd("Oil")
+
+-- auto open (within reason)
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        local argv = vim.fn.argv()
+        if #argv == 0 then
+            -- needs this bc it can open dir before nvim is ready unlike when provided an argument (says the LLMs)
+            vim.schedule(function()
+                vim.cmd("Oil .")
+            end
+            )
+        elseif vim.fn.isdirectory(argv[1]) == 1 then
+            vim.cmd("Oil " .. argv[1])
+        end
+    end,
+})
