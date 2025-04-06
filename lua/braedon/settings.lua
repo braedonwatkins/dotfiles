c508405fb984d623de08d99d64033312ea8b6be5
braedonwatkins <watkins.braedon@gmail.com>
2025-04-05 21:08:42 -0400

    settings

--- lua/braedon/settings.lua
+++ lua/braedon/settings.lua
@@ -1,3 +1,27 @@
+vim.g.mapleader = " "
+
 -- search highlighting when you want it
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+
+vim.opt.relativenumber = true
+vim.opt.nu = true
+
+--spacing
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+
+-- undotree stuff??
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- folding stuff
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldcolumn = "0"
