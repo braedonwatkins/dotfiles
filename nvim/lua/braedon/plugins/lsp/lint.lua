66d21a786adf32ce1391b9eb33f31bfd8f85b74b
braedonwatkins <watkins.braedon@gmail.com>
2025-04-07 08:58:41 -0400

    removing pylint, seems to not respect venv

--- nvim/lua/braedon/plugins/lsp/lint.lua
+++ nvim/lua/braedon/plugins/lsp/lint.lua
@@ -1,21 +1,20 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufWritePost", "BufReadPost", "InsertLeave" },
-  config = function()
-    local lint = require("lint")
-    
-    lint.linters_by_ft = {
-      javascript = { "eslint" },
-      typescript = { "eslint" },
-      javascriptreact = { "eslint" },
-      typescriptreact = { "eslint" },
-      python = { "pylint" },
-    }
-    
-    vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
-      callback = function()
-        require("lint").try_lint()
-      end,
-    })
-  end,
+    "mfussenegger/nvim-lint",
+    event = { "bufwritepost", "bufreadpost", "insertleave" },
+    config = function()
+        local lint = require("lint")
+
+        lint.linters_by_ft = {
+            javascript = { "eslint" },
+            typescript = { "eslint" },
+            javascriptreact = { "eslint" },
+            typescriptreact = { "eslint" },
+        }
+
+        vim.api.nvim_create_autocmd({ "bufwritepost", "bufreadpost", "insertleave" }, {
+            callback = function()
+                require("lint").try_lint()
+            end,
+        })
+    end,
 }
--- nvim/lua/braedon/plugins/lsp/servers.lua
+++ nvim/lua/braedon/plugins/lsp/servers.lua
@@ -1,107 +1,108 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-	},
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+    },
 
-	config = function()
-		local lspconfig = require("lspconfig")
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    config = function()
+        local lspconfig = require("lspconfig")
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			-- special server configs
-			["lua_ls"] = function()
-				lspconfig.lua_ls.setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
-				})
-			end,
-			["volar"] = function()
-				lspconfig.volar.setup({
-					capabilities = capabilities,
-					filetypes = { "vue" },
-				})
-			end,
-			-- FIXME: for when i do c++/embedded
-			-- ["ccls"] = function()
-			-- 	lspconfig.ccls.setup({
-			-- 		capabilities = capabilities,
-			-- 		flags = {
-			-- 			debounce_text_changes = 150,
-			-- 		},
-			-- 		init_options = {
-			-- 			compilationDatabaseDirectory = "build",
-			-- 			cache = {
-			-- 				directory = "/tmp/ccls-cache",
-			-- 			},
-			-- 		},
-			-- 	})
-			-- end,
-		})
+        require("mason-lspconfig").setup_handlers({
+            function(server_name)
+                lspconfig[server_name].setup({
+                    capabilities = capabilities,
+                })
+            end,
+            -- special server configs
+            ["lua_ls"] = function()
+                lspconfig.lua_ls.setup({
+                    capabilities = capabilities,
+                    settings = {
+                        Lua = {
+                            diagnostics = {
+                                globals = { "vim" },
+                            },
+                        },
+                    },
+                })
+            end,
+            ["volar"] = function()
+                lspconfig.volar.setup({
+                    capabilities = capabilities,
+                    filetypes = { "vue" },
+                })
+            end,
+            -- FIXME: for when i do c++/embedded
+            -- ["ccls"] = function()
+            -- 	lspconfig.ccls.setup({
+            -- 		capabilities = capabilities,
+            -- 		flags = {
+            -- 			debounce_text_changes = 150,
+            -- 		},
+            -- 		init_options = {
+            -- 			compilationDatabaseDirectory = "build",
+            -- 			cache = {
+            -- 				directory = "/tmp/ccls-cache",
+            -- 			},
+            -- 		},
+            -- 	})
+            -- end,
+        })
 
-		local cmp = require("cmp")
+        local cmp = require("cmp")
 
-		cmp.setup({
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "buffer" },
-				{ name = "path" },
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
-				["<Tab>"] = cmp.mapping.select_next_item(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
-		})
+        cmp.setup({
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "buffer" },
+                { name = "path" },
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                ["<Tab>"] = cmp.mapping.select_next_item(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+        })
 
-		-- Enable autocompletion
-		vim.o.completeopt = "menuone,noselect"
+        -- Enable autocompletion
+        vim.o.completeopt = "menuone,noselect"
 
-		-- Diagnostic configuration
-		vim.diagnostic.config({
-			virtual_text = true,
-			signs = true,
-			update_in_insert = false,
-			underline = true,
-			severity_sort = true,
-			float = {
-				focusable = true,
-				style = "minimal",
-				border = "rounded",
-				header = "",
-				prefix = "",
-			},
-		})
+        -- Diagnostic configuration
+        vim.diagnostic.config({
+            virtual_text = true,
+            signs = true,
+            update_in_insert = false,
+            underline = true,
+            severity_sort = true,
+            float = {
+                focusable = true,
+                style = "minimal",
+                border = "rounded",
+                header = "",
+                prefix = "",
+            },
+        })
 
-		-- lsp defined keymappings  TODO: see if these can be better abstracted
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
-				local opts = { buffer = ev.buf }
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-			end,
-		})
-	end,
+        -- lsp defined keymappings  TODO: see if these can be better abstracted
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+            callback = function(ev)
+                local opts = { buffer = ev.buf }
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+            end,
+        })
+    end,
 }
 -- Set up lint events
