713618d0ff9470245dc4b16d3dd0e209aa0551b2
braedonwatkins <watkins.braedon@gmail.com>
2026-01-25 11:17:58 -0500

    telescope fzf go brrr

--- nvim/lua/braedon/plugins/telescope.lua
+++ nvim/lua/braedon/plugins/telescope.lua
@@ -1,6 +1,10 @@
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
     config = function()
         local telescope = require("telescope.builtin")
         vim.keymap.set("n", "<leader>pf", function()
