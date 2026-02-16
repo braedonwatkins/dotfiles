6cea5137115e3be5d15348fc565c6b81dbd43774
braedonwatkins <watkins.braedon@gmail.com>
2026-02-16 13:14:38 -0500

    better diffview keys

--- nvim/lua/braedon/plugins/diffview.lua
+++ nvim/lua/braedon/plugins/diffview.lua
@@ -1,4 +1,8 @@
-vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>do", ":DiffviewOpen<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>dc", ":DiffviewClose<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>dh", ":DiffviewFileHistory %<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>db", ":DiffviewOpen origin/main...HEAD<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>dr", ":DiffviewOpen --rebase<CR>", { noremap = true, silent = true })
 
 return {
     "sindrets/diffview.nvim",
